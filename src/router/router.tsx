5e8592cf44bacdcd0732fee51af2bb08393c1e23
Minna <minnalnh@gmail.com>
2026-05-28 22:48:28 +0200

    added portfolio ending in router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -5,7 +5,7 @@ import ErrorPage from "../pageSections/ErrorPage";
 
 export const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/portfolio",
     element: <Layout />,
     errorElement: <ErrorPage />,
     children: [
